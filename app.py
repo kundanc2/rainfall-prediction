4079c2f30e0aec870192b2c0505c504a2448f44d
kundanc2 <145100364+kundanc2@users.noreply.github.com>
2023-10-16 11:22:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import streamlit as st
 import joblib
-
+import matplotlib.pyplot as plt
 
 
 # Load dataset
@@ -60,4 +60,57 @@ if st.button("Predict Rainfall"):
         else:
             st.error("No model available for the specified month and year.")
         
+if st.checkbox("Plot Yearly Rainfall Prediction"):
+    month_num_input = Month_map.get(month_input, None)
+    if month_num_input is None:
+        st.error("Invalid month input.")
+    else:
+        subdivision_encoded = label_encoder.transform([subdivision_input])
 
+        # Initialize lists to store monthly predictions
+        yearly_predictions = []
+        for month in range(1, 13):
+            input_features = np.array([[year_input, subdivision_encoded[0]]])
+            model = models.get(month, None)
+            if model:
+                predicted_rainfall = model.predict(input_features)
+                yearly_predictions.append(predicted_rainfall[0])
+            else:
+                yearly_predictions.append(0.0)  # Handle the case when the model is not available
+
+        # Plot the yearly prediction
+        fig, ax = plt.subplots()
+        ax.plot(list(Month_map.keys()), yearly_predictions, marker='o')
+        ax.set(xlabel='Month', ylabel='Rainfall (mm)', title=f"Yearly Rainfall Prediction for {year_input} in {subdivision_input}")
+        st.pyplot(fig)
+
+if st.checkbox("Plot years data of Subdivison"):
+    subdivision_encoded = label_encoder.transform([subdivision_input])
+    
+    # Initialize lists to store yearly predictions
+    yearly_predictions = []
+    years = range(year_input - 10, year_input)  # Get the past 5 years
+    for year in years:
+        # Initialize a list to store monthly predictions for the current year
+        monthly_predictions = []
+        for month in range(1, 13):
+            input_features = np.array([[year, subdivision_encoded[0]]])
+            model = models.get(month, None)
+            if model:
+                predicted_rainfall = model.predict(input_features)
+                monthly_predictions.append(predicted_rainfall[0])
+                
+            else:
+                monthly_predictions.append(0.0) 
+                # Handle the case when the model is not available
+        
+        # Calculate the yearly average for the current year
+        yearly_avg = sum(monthly_predictions) / 12
+        
+        yearly_predictions.append(yearly_avg)
+
+    # Plot the yearly prediction
+    fig, ax = plt.subplots()
+    ax.plot(years, yearly_predictions, marker='o')
+    ax.set(xlabel='Year', ylabel='Yearly Rainfall (mm)', title=f"Yearly Rainfall Prediction for {subdivision_input} (Last 5 Years)")
+    st.pyplot(fig)
